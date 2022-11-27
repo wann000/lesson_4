6781845350627bed196335f7aa0e608275f01aae
wann000 <wann0@yandex.ru>
2022-11-28 00:00:19 +0300

    4

--- 25/Program.cs
+++ 25/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+//Console.Clear();
+System.Console.WriteLine();
+
+int number = 2;
+int exponent = 10;
+int count = 1;
+int result = number;
+
+while (count < exponent)
+{
+    result = result * number;
+    count++;
+}
+
+System.Console.WriteLine($"{number} ^ {exponent} = {result}");
+System.Console.WriteLine();
