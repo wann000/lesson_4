eb2d0517934b4620e6f494ab7b9c1ab316207848
wann000 <wann0@yandex.ru>
2022-11-28 00:17:27 +0300

    27

--- 27/Program.cs
+++ 27/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Clear();
+System.Console.WriteLine();
+
+int number = new Random().Next(1, 100000);
+int sumDigit = 0;
+System.Console.Write($"Сумма цифр числа {number} равна ");
+
+while (number > 0)
+{
+    int digit = number % 10;
+    sumDigit = sumDigit + digit;
+    number = number / 10;
+}
+
+System.Console.WriteLine(sumDigit);
+System.Console.WriteLine();
